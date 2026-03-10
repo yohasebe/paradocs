94bcef09770565be4287026d57c4e79aff7cafec
Yoichiro Hasebe <yohasebe@gmail.com>
2026-03-11 07:08:55 +0900

    Remove legacy Sinatra code, migrate to static site only
    Sinatra server-side code archived to local _legacy/ directory (not tracked).
    Repository now contains only the static site (docs/) and build tooling.

--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-ENV['RACK_ENV'] = 'test'
-
-require 'minitest/autorun'
-require 'rack/test'
-
-$LOAD_PATH.unshift File.expand_path('..', __dir__)
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
